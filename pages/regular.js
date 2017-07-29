fe00907857b454e02fe1752cb8c65f9cfa85214d
jmsv23 <jmsv23@gmail.com>
2017-07-29 13:28:51 -0500

    Init

---
+++ pages/regular.js
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+
+class Regular extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <p>Regular React Component.</p>
+      </div>
+    )
+  }
+}
+
+export default Regular
