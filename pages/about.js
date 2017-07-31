f1747863252c21779c91d44147f295061d08b7ad
jmsv23 <jmsv23@gmail.com>
2017-07-31 17:17:35 -0500

    url mask

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Link from 'next/link'
 import Layout from '../components/layout'
 
-const About = () => (
+const About = (props) => (
   <Layout>
     <h2>Next.js React Without Complications.</h2>
     <h3>Drupalcamp Costa Rica 2017</h3>
     <p>Ing. Jose Manuel Santibañez Villanueva</p>
+    {typeof props.url.query.test !== 'undefined' ?
+      <h1>{props.url.query.test}</h1>
+      :
+      <Link as={`/about/funciona`} href={`/about?test=funciona`}>
+        <a className="btn btn-default">about/funciona</a>
+      </Link>
+    }
   </Layout>
 )
 
